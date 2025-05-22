7ae94d24d7eaeb10881a88ca68ff610b06e8c1d4
remondawagwwh <remondawageh88@gmail.com>
2025-05-22 17:30:37 +0300

    create app book

--- blog/book/views.py
+++ blog/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home (request):
+    return HttpResponse("Home page")
+
+def users (request):
+    return HttpResponse("user page")
+
+def about (request):
+    return HttpResponse("about page")
